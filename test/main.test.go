edf10314d3cfa59dd83ed0f38c068deeec033163
emersonamorim-dev <emerson_tecno@hotmail.com>
2023-08-02 10:56:14 -0300

    PaymentGateway-Golang

--- test/main.test.go
+++ test/main.test.go
@@ -1,34 +1,113 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"strings"
+	"paymentgateway-golang/pkg/payments/credit"
+	"paymentgateway-golang/pkg/payments/debit"
+	"paymentgateway-golang/pkg/payments/pix"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
 
-func TestPaymentEndpoint(t *testing.T) {
-	r := gin.Default()
-	r.POST("/payment", func(c *gin.Context) {
-		c.JSON(202, gin.H{
-			"message": "O pagamento está sendo processado",
-		})
-	})
+func TestProcessDebitCardPayment(t *testing.T) {
+	// Criação do contexto de teste
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/payment/debit", processDebitCardPayment)
 
-	req, err := http.NewRequest("POST", "/payment", strings.NewReader(`{"amount": 100}`))
-	if err != nil {
-		t.Fatalf("Não foi possível criar a solicitação: %v", err)
+	// Dados de pagamento para teste
+	payment := debit.DebitPayment{
+		CardNumber: "1234567890123456",
+		Amount:     100,
 	}
+
+	// Converte os dados de pagamento para JSON
+	jsonData, _ := json.Marshal(payment)
+
+	// Simula uma requisição HTTP POST para a rota
 	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
+	req, _ := http.NewRequest("POST", "/payment/debit", bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
 
-	if w.Code != http.StatusAccepted {
-		t.Fatalf("Espere para obter status %d but instead got %d", http.StatusAccepted, w.Code)
+	// Verifica o status code esperado
+	if w.Code != 202 {
+		t.Errorf("Esperado código 202, mas obteve %d", w.Code)
 	}
 
-	if w.Body.String() != `{"message":"O pagamento está sendo processado"}` {
-		t.Fatalf("Espere para receber mensagem '%s' but instead got '%s'", `{"message":"O pagamento está sendo processado"}`, w.Body.String())
+	// Verifica a resposta esperada
+	expectedResponse := `{"message":"O pagamento com cartão de débito está sendo processado"}`
+	if w.Body.String() != expectedResponse {
+		t.Errorf("Resposta esperada %s, mas obteve %s", expectedResponse, w.Body.String())
+	}
+}
+
+func TestProcessCreditCardPayment(t *testing.T) {
+	// Criação do contexto de teste
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/payment/credit", processCreditCardPayment)
+
+	// Dados de pagamento para teste
+	payment := credit.CreditPayment{
+		CardNumber: "1234567890123456",
+		Amount:     100,
+	}
+
+	// Converte os dados de pagamento para JSON
+	jsonData, _ := json.Marshal(payment)
+
+	// Simula uma requisição HTTP POST para a rota
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/payment/credit", bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
+
+	// Verifica o status code esperado
+	if w.Code != 202 {
+		t.Errorf("Esperado código 202, mas obteve %d", w.Code)
+	}
+
+	// Verifica a resposta esperada
+	expectedResponse := `{"message":"O pagamento com cartão de crédito está sendo processado"}`
+	if w.Body.String() != expectedResponse {
+		t.Errorf("Resposta esperada %s, mas obteve %s", expectedResponse, w.Body.String())
+	}
+}
+
+func TestProcessPixPayment(t *testing.T) {
+	// Criação do contexto de teste
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/payment/pix", processPixPayment)
+
+	// Dados de pagamento para teste
+	payment := pix.PixPayment{
+		PixKey:  "chave_pix",
+		Amount:  100,
+	}
+
+	// Converte os dados de pagamento para JSON
+	jsonData, _ := json.Marshal(payment)
+
+	// Simula uma requisição HTTP POST para a rota
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/payment/pix", bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
+
+	// Verifica o status code esperado
+	if w.Code != 202 {
+		t.Errorf("Esperado código 202, mas obteve %d", w.Code)
+	}
+
+	// Verifica a resposta esperada
+	expectedResponse := `{"message":"Pagamento com Pix está sendo processado"}`
+	if w.Body.String() != expectedResponse {
+		t.Errorf("Resposta esperada %s, mas obteve %s", expectedResponse, w.Body.String())
 	}
 }
